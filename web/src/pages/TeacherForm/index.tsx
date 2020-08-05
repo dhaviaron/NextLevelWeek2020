ac5a90290474b8f2e71bbc10ca6354a6de975d7b
DAWestphal <56050283+DAWestphal@users.noreply.github.com>
2020-08-05 19:48:49 -0300

    Mudanças até 19:50

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,61 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import './styles.css'
+import Input from '../../components/Input';
+import warningIcon from '../../assets/images/icons/warning.svg'
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas." />
+      <PageHeader 
+      title="Que incrível que você quer dar aulas." 
+      description="O primeiro passo é preencher este formulário"
+      />
+
+      <main>
+        <fieldset>
+          <legend> Seus dados</legend>
+          <Input name="name" label="Nome Completo"/>
+          <Input name="avatar" label="Avatar"/>
+          <Input name="Whatsapp" label="Whatsapp"/> 
+          <Textarea name="bio" label="Biografia"/>
+        </fieldset>
+        <fieldset>
+          <legend> Sobre a aula</legend>
+          <Select 
+            name="subject" 
+            label="Matéria" 
+            options={[
+              {value: "Artes", label: "Artes"},
+              {value: "Biologia", label: "Biologia"},
+              {value: "Ciências", label: "Ciências"},
+              {value: "Educação física", label: "Educação física"},
+              {value: "Física", label: "Física"},
+              {value: "Geografia", label: "Geografia"},
+              {value: "História", label: "História"},
+              {value: "Matemática", label: "Matemática"},
+              {value: "Português", label: "Português"},
+              {value: "Química", label: "Química"},
+            ]}
+          />
+          
+          <Input name="cost" label="Custo/hora da aula" />         
+        </fieldset>
+        <fieldset>
+          <legend>Horários Disponíveis</legend>
+        </fieldset>
+        <footer>
+        <p>
+         <img src={warningIcon} alt="Aviso Importante"/>
+         Importante <br/>
+         Preencha todos os dados
+        </p> 
+        <button type="button">Salvar Cadastro</button>
+        </footer>
+      </main>
     </div>
   );
 }
